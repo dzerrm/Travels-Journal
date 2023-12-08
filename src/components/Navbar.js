315d0af218465a717df5b206ac384a862285eda5
dzerrm <alexonnn2003@gmail.com>
2023-12-08 04:44:01 +0200

    modified:   README.md 	modified:   src/App.js 	new file:   src/components/Navbar.js 	modified:   src/index.js

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function Navbar(){
+    return(
+        <div className ="navbar">
+            <img src="" />
+            <span className="navbar--text">My travels journal</span>
+        </div>
+    );
+}
